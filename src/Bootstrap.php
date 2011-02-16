0ca03cc0e03f9c5df49fa80e7127b196cbeba116
Tim de Pater <git@trafex.nl>
2011-02-16 09:31:11 +0100

    Added bootstrap for autoloading.

---
+++ src/Bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * TX-Spider
+ *
+ * @category   Spider
+ * @package    Boostrap
+ * @copyright  Copyright (c) 2011 Tim de Pater
+ * @license    http://www.gnu.org/licenses/gpl.txt GPLv3 License
+ * @version    $Id:$
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+date_default_timezone_set('Europe/Amsterdam');
+
+require_once 'src/Spider.php';
+require_once 'src/CoucheDb.php';
+
+/**
+ * Convenience function for exiting on failure
+ *
+ * @param string $message
+ * @return void
+ */
+function failure($message)
+{
+    echo $message . PHP_EOL;
+    exit(200); // Non-zero value denotes an error, above 128 is user defined
+}
+
+// Autoload the ZF classes
+function zendFrameworkAutoload($class)
+{
+    require_once str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+}
+spl_autoload_register('zendFrameworkAutoload');
